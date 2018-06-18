2e9e91923e1798669b4b09212c1899767b4ac3ca
YigaoFan <fanyigao@qq.com>
2018-06-18 09:21:16 +0800

    fix the air block
    As the title.

--- 3rd/block.js
+++ 3rd/block.js
@@ -19,7 +19,9 @@ var Block = function(game, position) {
 		}
 	}
 
-	
+	var aInb = function(x, x1, x2) {
+		return x >= x1 && x <= x2
+	}
 	o.collide = function(ball) {
 		//if (ball.y + ball.h> o.y) {
 		//	if (ball.x > o.x && ball.x < o.w+ o.x) {
@@ -30,21 +32,20 @@ var Block = function(game, position) {
 		//return false
 		var a = o
 		var b = ball
-		if (aInb(a.x, b.x, b.x+b.w) && aInb(a.y, b.y, b.y+b.h)) {
-			return true
-		} else if (aInb(b.x, a.x, a.x+a.w) && aInb(b.y, a.y, a.y+a.h)) {
-			return true
-		} else {
-			return false
+		if (o.alive) {
+			if (aInb(a.x, b.x, b.x+b.w) && aInb(a.y, b.y, b.y+b.h)) {
+				return true
+			} else if (aInb(b.x, a.x, a.x+a.w) && aInb(b.y, a.y, a.y+a.h)) {
+				return true
+			} else {
+				return false
+			}
 		}
 		//if (aInb(a.x, b.x, b.x+b.w) || aInb(b.x, a.x, a.x+a.w)) {
 		//	if (aInb(a.y, b.y, b.y+b.h) || aInb(b.y, a.y, a.y+a.h)) {
 		//		return true
 		//	}
 		//}
-	}
-	var aInb = function(x, x1, x2) {
-		return x >= x1 && x <= x2
 	}
 	return o
 }
